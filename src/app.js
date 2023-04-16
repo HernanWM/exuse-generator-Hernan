76024e1b83f6447db98ff0db8d2d8ff147053e7f
HernanWM <Waza.mango@gmail.com>
2023-04-16 00:23:54 +0000

    changes

--- src/app.js
+++ src/app.js
@@ -6,6 +6,34 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = exuseGenerator();
+};
+
+const exuseGenerator = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  const excuseOne = [Math.floor(Math.random() * who.length)];
+  const excuseTwo = [Math.floor(Math.random() * action.length)];
+  const excuseThree = [Math.floor(Math.random() * what.length)];
+  const excuseFour = [Math.floor(Math.random() * when.length)];
+
+  return (
+    who[excuseOne] +
+    " " +
+    action[excuseTwo] +
+    " " +
+    what[excuseThree] +
+    " " +
+    what[excuseThree] +
+    " " +
+    when[excuseFour]
+  );
 };
